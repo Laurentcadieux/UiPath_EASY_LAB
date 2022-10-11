57fd80abf7ca4e85ca844061e3e49d7a153f54e3
Laurentcadieux <46010332+Laurentcadieux@users.noreply.github.com>
2022-10-11 08:18:12 -0400

    update

--- L1_master_win_dc_sql.ps1
+++ L1_master_win_dc_sql.ps1
@@ -1,4 +1,3 @@
-echo "hello world"
 # Install AD DS and AD DNS
 ### Open Pwershell as Administrator 
 ### Install key services needed 
@@ -7,8 +6,8 @@ Add-WindowsFeature AD-Domain-Services, DNS -IncludeManagementTools
 #install chocolatey CHEAT CODE
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 #via chocolatey install sql-server with tools CHEAT CODE
-choco install sql-server-2019 -y  /IsoPath:"c:\downloads\SQLServer2019-x64-ENU-Dev.iso"
 netsh advfirewall firewall add rule name="SQL Server 1433" dir=in action=allow protocol=TCP localport=1433 profile=any enable=yes service=any
 #via chocolatey install SQL Studio 
 choco install sql-server-management-studio -y	
 choco install putty.install --version=0.71 -y
+shutdown -r
---
+++ L1_master_win_dc_sql_2.ps1
@@ -0,0 +1,4 @@
+#via chocolatey install SQL Studio 
+choco install sql-server-2019 -y
+
+shutdown -r
