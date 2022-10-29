84e047b3d1b960223141cb7cc3c51bf09315a4c6
Laurentcadieux <46010332+Laurentcadieux@users.noreply.github.com>
2022-10-29 10:01:03 -0400

    regrouped

--- L1_Master_azuresetup_template.ps1
+++ L1_Master_azuresetup_template.ps1
@@ -15,9 +15,6 @@ $subnetfirewall = "10.1.0.64/26"
 $subnamemain = "main"
 $subnetmain = "10.1.0.0/26"
 
-
-
-
 $vm =  "azlab"
 $user =  "uipadmin"
 ## The password length must be between 12 and 123. Password must have the 3 of the following: 1 lower case character, 1 upper case character, 1 number and 1 special character.
@@ -38,6 +35,7 @@ az group create --location $region --name $resourceGroup  --tags owner=XXXXXXXX@
 az network vnet create --name $vnet --resource-group $resourceGroup --address-prefix $subnetiprange --subnet-name $subnamemain  --subnet-prefixes $subnetmain
 ##  ************************** Create subnet bastion ************************************
 az network vnet subnet create -g $resourceGroup --vnet-name $vnet -n $subnamebas  --address-prefix $subnetbastion
+
 ##  ************************** Create subnet firewall ************************************
 az network vnet subnet create -g $resourceGroup --vnet-name $vnet -n $subnamefw  --address-prefix $subnetfirewall
 ##  ************************** Create subnet gateway ************************************
@@ -49,7 +47,7 @@ az network public-ip create --resource-group $resourceGroup --name $publicITbast
 az network bastion create --name MyBastion --public-ip-address $publicITbastion --resource-group $resourceGroup --vnet-name $vnet --location $region
 
 
-##  ************************** Create Windows VM  ************************************
+##  ************************** Create Windows VM No Public IP in the main subnet  ************************************
 az vm create --resource-group $resourceGroup  --name "$vm-dcsql01s" --location $region --size Standard_D2_v4  --admin-username $user  --admin-password $pwd  --image $imageraw --vnet-name $vnet  --subnet $subnamemain --zone 1  --private-ip-address $staticIP --public-ip-address '""'
 ##  ************************** Windows VM install step 1  ************************************
 az vm run-command invoke -g $resourceGroup -n "$vm-dcsql01s" --command-id RunPowerShellScript --scripts "[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/Laurentcadieux/UiPath_EASY_LAB/main/L1_master_win_dc_sql.ps1'))"
